16d0904c17c34a1e996301a62a19da1cee432ee1
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-10-15 20:51:52 -0300

    Feat: Implemented Step form Navigation Button in navbar

--- src/layouts/StepFormLayout/index.tsx
+++ src/layouts/StepFormLayout/index.tsx
@@ -1,17 +1,58 @@
 import { Outlet } from 'react-router-dom'
-import { SidebarItem } from '../../components/SidebarItem'
-import { Sidebar, StepFormLayoutContainer, Steps } from './styles'
+import {
+  NumberSquareFour,
+  NumberSquareOne,
+  NumberSquareThree,
+  NumberSquareTwo,
+} from 'phosphor-react'
+
+import { usePaintCalcFormControls } from '../../hooks/usePaintCalcFormControls'
+
+import {
+  Icon,
+  Point,
+  Sidebar,
+  SidebarItem,
+  StepFormLayoutContainer,
+} from './styles'
 
 export function StepFormLayout() {
+  const { formStep1, formStep2, formStep3, formStep4 } =
+    usePaintCalcFormControls()
+
   return (
     <StepFormLayoutContainer>
-      <Steps>
-        <Sidebar>
-          <SidebarItem />
-        </Sidebar>
+      <Sidebar>
+        <SidebarItem end to={'step1'} variant={formStep1}>
+          <Icon>
+            <NumberSquareOne size={32} />
+          </Icon>
+          <Point />
+        </SidebarItem>
 
-        <Outlet />
-      </Steps>
+        <SidebarItem to={formStep1 ? 'step2' : '#'} variant={formStep2}>
+          <Icon>
+            <NumberSquareTwo size={32} />
+          </Icon>
+          <Point />
+        </SidebarItem>
+
+        <SidebarItem to={formStep2 ? 'step3' : '#'} variant={formStep3}>
+          <Icon>
+            <NumberSquareThree size={32} />
+          </Icon>
+          <Point />
+        </SidebarItem>
+
+        <SidebarItem to={formStep3 ? 'step4' : '#'} variant={formStep4}>
+          <Icon>
+            <NumberSquareFour size={32} />
+          </Icon>
+          <Point />
+        </SidebarItem>
+      </Sidebar>
+
+      <Outlet />
     </StepFormLayoutContainer>
   )
 }
--- src/layouts/StepFormLayout/styles.ts
+++ src/layouts/StepFormLayout/styles.ts
@@ -1,15 +1,64 @@
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
-export const StepFormLayoutContainer = styled.div``
-
-export const Steps = styled.div`
-  flex: 1;
+export const StepFormLayoutContainer = styled.div`
   display: flex;
+  flex: 1;
 `
 
 export const Sidebar = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border-right: 1px solid ${(props) => props.theme.colors['gray-300']};
   padding-left: 1rem;
   padding-right: 1.5rem;
   border-top: 1px solid ${(props) => props.theme.colors['gray-300']};
+`
+
+interface SidebarItemProps {
+  variant: 0 | 1 | 2 | 3 | 4
+}
+
+export const SidebarItem = styled(NavLink)`
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  &.active {
+    div:last-child {
+      background-color: ${(props) => props.theme.colors['orange-500']};
+      border: 3px solid ${(props) => props.theme.colors['gray-800']};
+    }
+  }
+`
+
+export const Icon = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 6px;
+  background-color: ${(props) => props.theme.colors['gray-300']};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  svg {
+    color: ${(props) => props.theme.colors['gray-100']};
+  }
+`
+
+export const Point = styled.div`
+  width: 0.375rem;
+  height: 0.375rem;
+  padding: 0.375rem;
+  border: 3px solid ${(props) => props.theme.colors['gray-300']};
+  border-radius: 50%;
+  margin-left: 3.5rem;
+  cursor: pointer;
+  z-index: 2;
+  position: absolute;
+  background: ${(props) => props.theme.colors['gray-100']};
 `
